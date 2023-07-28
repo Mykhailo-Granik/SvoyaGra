aa2e2739c388139fc961996cc541f2c6fea8e37d
Mykhailo Granik <mgranik@axcient.com>
2023-07-28 20:22:26 +0300

    Introduced the TournamentParser and it's first empty implementation

---
+++ src/main/java/com/svoyagra/parser/ShvagerLeagueParser.java
@@ -0,0 +1,12 @@
+package com.svoyagra.parser;
+
+import com.svoyagra.domain.Tournament;
+
+import java.util.Objects;
+
+public class ShvagerLeagueParser implements TournamentParser {
+    @Override
+    public Tournament parse() {
+        return null;
+    }
+}
---
+++ src/main/java/com/svoyagra/parser/TournamentParser.java
@@ -0,0 +1,9 @@
+package com.svoyagra.parser;
+
+import com.svoyagra.domain.Tournament;
+
+public interface TournamentParser {
+
+    Tournament parse();
+
+}
